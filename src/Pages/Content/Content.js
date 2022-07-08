1330c02874c0d624d51f62a167ecf551fb5f9a7c
Amit Goyal <agoyal3_be21@thapar.edu>
2022-07-08 21:09:32 +0530

    minor changes

--- src/Pages/Content/Content.js
+++ src/Pages/Content/Content.js
@@ -12,49 +12,49 @@ function Content () {
       <div className="content-container container-fluid">
         <div className="row content justify-content-center align-items-center">
           {/* About Us */}
-          <div className="about row col-11 my-4 p-4 justify-content-center align-items-center">
+          <div className="about row col-8 col-lg-11 my-4 p-4 justify-content-center align-items-center">
             <h1 className="text-center">About Us</h1>
-            <div className="content-text col-5 text-center">
+            <div className="content-text col-md-5 col-10 text-center order-2 order-md-1">
               <h3>Who we are</h3>
               <p>
                 WHO WE ARE
                 Thapar Food Festival is one of the most magnificient and pompous fests of Thapar Institute of Engineering and Technology. The fest takes place in September and with Coca-Cola as our title sponsor, we hope mesmerise more people by the beauty of food .
               </p>
-              <h3 className="ms-5">THE PALLETE OF FLAVOURS</h3>
+              <h3 className="">THE PALLETE OF FLAVOURS</h3>
               <p>
                 The theme, a palette of flavors is an idea revolving around the concept that basic flavors blend to give rise to an assortment of flavors bringing about the diversity in food. The motive is to bring to light, foods that are visually appealing and exquisitely flavoured.
               </p>
             </div>
-            <div className="logo col-5">
+            <div className="logo col-md-5 col-10 order-1 order-md-2">
               <img src={Logo} alt="" className="img-fluid" />
             </div>
           </div>
 
           {/* Past Sponsors */}
-          <div className="about row col-11 my-5 p-4 justify-content-center align-items-center">
+          <div className="about row col-lg-11 col-10 my-5 p-4 justify-content-center align-items-center">
             <h1 className="text-center">Past Sponsors</h1>
-            <div className="sponsors row justify-content-center align-items-center p-4">
-              <img src={Coke} className="col-2 m-2 img-fluid" alt="coke" />
-              <img src={Keventer} className="col-2 m-2 img-fluid" alt="" />
-              <img src={Coke} className="col-2 m-2 img-fluid" alt="coke" />
-              <img src={Keventer} className="col-2 m-2 img-fluid" alt="" />
-              <img src={Coke} className="col-2 m-2 img-fluid" alt="coke" />
-              <img src={Keventer} className="col-2 m-2 img-fluid" alt="" />
-              <img src={Coke} className="col-2 m-2 img-fluid" alt="coke" />
-              <img src={Keventer} className="col-2 m-2 img-fluid" alt="" />
+            <div className="sponsors row justify-content-center align-items-center p-lg-4 p-2">
+              <img src={Coke} className="col-lg-2 col-4 m-lg-2 m-1 img-fluid" alt="coke" />
+              <img src={Keventer} className="col-lg-2 col-4 m-lg-2 m-1 img-fluid" alt="" />
+              <img src={Coke} className="col-lg-2 col-4 m-lg-2 m-1 img-fluid" alt="coke" />
+              <img src={Keventer} className="col-lg-2 col-4 m-lg-2 m-1 img-fluid" alt="" />
+              <img src={Coke} className="col-lg-2 col-4 m-lg-2 m-1 img-fluid" alt="coke" />
+              <img src={Keventer} className="col-lg-2 col-4 m-lg-2 m-1 img-fluid" alt="" />
+              <img src={Coke} className="col-lg-2 col-4 m-lg-2 m-1 img-fluid" alt="coke" />
+              <img src={Keventer} className="col-lg-2 col-4 m-lg-2 m-1 img-fluid" alt="" />
             </div>
           </div>
           {/* Memories */}
-          <div className="about row col-11 my-5 p-4 justify-content-center align-items-center">
+          <div className="about row col-lg-11 col-10 my-5 p-4 justify-content-center align-items-center">
             <h1 className="text-center">Memories</h1>
             <p className="text-center col-8 my-4">
               Memories are treasures one will always preserve. One may not live a moment again, but can remember and cherish it forever. Memoreies are windows to the past that allow one to jump into that momentous joy and rekive it again. Thapar Food Festival is a joy of many and has always created a plethora of memories, that cherish this unique concoction of food,fun and satisfaction. Music, Art, Dance, Sweet, Sour-our bag is a mixture of all emotions. Below are glimpse of the same:
             </p>
-            <div className="memories row justify-content-center align-items-center p-4">
-              <img src={TFF_16} className="col-4 img-fluid m-2" alt="" />
-              <img src={TFF_16} className="col-4 img-fluid m-2" alt="" />
-              <img src={TFF_16} className="col-4 img-fluid m-2" alt="" />
-              <img src={TFF_16} className="col-4 img-fluid m-2" alt="" />
+            <div className="memories row justify-content-center align-items-center p-4 col-10">
+              <img src={TFF_16} className="col-lg-4 col-5 img-fluid m-2" alt="" />
+              <img src={TFF_16} className="col-lg-4 col-5 img-fluid m-2" alt="" />
+              <img src={TFF_16} className="col-lg-4 col-5 img-fluid m-2" alt="" />
+              <img src={TFF_16} className="col-lg-4 col-5 img-fluid m-2" alt="" />
             </div>
           </div>
 
--- src/Pages/Footer/Footer.js
+++ src/Pages/Footer/Footer.js
@@ -7,8 +7,8 @@ function Footer () {
   return (
     <Fragment>
       <div className="footer-container justify-content-center align-items-center container-fluid row">
-        <div className="row content justify-content-evenly align-items-start col-12 ">
-          <div className="contact_us col-2">
+        <div className="row content justify-content-lg-evenly justify-content-center align-items-start col-12 ">
+          <div className="contact_us col-2 text-center">
 
             <h4>Contact Us</h4>
             <p>
@@ -18,15 +18,15 @@ function Footer () {
               Email → <br />thaparfoodfestival22@gmail.com
             </span>
           </div>
-          <div className="facebook col-2 mx-2 px-3">
+          <div className="facebook col-2 text-center mx-2 px-3">
             <h4>Facebook</h4>
             <img src={Fb} alt="" />
           </div>
-          <div className="facebook col-2 mx-2 px-3">
+          <div className="facebook col-2 text-center mx-2 px-3">
             <h4>Instagram</h4>
             <img src={Fb} alt="" />
           </div>
-          <div className="facebook col-2 mx-2 px-3">
+          <div className="facebook col-2 text-center mx-2 px-3">
             <h4>Location</h4>
             <img src={Fb} alt="" />
           </div>
